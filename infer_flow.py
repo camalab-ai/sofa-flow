ed5c92aaf507dd894b1e2ed5789225ea69777b8e
emilewa <45316859+emilewa@users.noreply.github.com>
2025-03-05 13:47:18 +0100

    Update and rename infer_flow_TMI.py to infer_flow.py

--- infer_flow.py
+++ infer_flow.py
@@ -8,7 +8,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 from networks import loadModels, getOpticalFlow
-from infer_TMI_seq import TMI_data
+from infer_seq import SOFA_data
 
 def load_image(imfile):
     img = np.array(Image.open(imfile)).astype(np.uint8)
@@ -23,7 +23,7 @@ if __name__ == '__main__':
     parser.add_argument('--DATA_PATH', help="data path", default='/home/emilia/DATASETS/Vident-real-100')
     parser.add_argument('--SPLIT', help="split", default='test')
     parser.add_argument('--IMAGE_DIR', help="image dir", default='GT')
-    parser.add_argument('--SAVE_DATASET_DIR', help="save dataset", default='test_TMI_data_C1')
+    parser.add_argument('--SAVE_DATASET_DIR', help="save dataset", default='test_data_C1')
 
 
     parser.add_argument('--small', action='store_true', help='use small model')
@@ -71,14 +71,14 @@ if __name__ == '__main__':
             'OF13': flow_f_13.permute(0, 2, 3, 1).cpu().numpy().astype(np.float32),
             'OF21': flow_b.permute(0, 2, 3, 1).cpu().numpy().astype(np.float32)})
 
-        print('TMI_data created ', seq)
-        tmi_data = TMI_data(data=infer_data_seq, model=args.MODEL, T=300, t=1.0)
+        print('SOFA_data created ', seq)
+        sofa_data = SOFA_data(data=infer_data_seq, model=args.MODEL, T=300, t=1.0)
 
         print('Selecting frames C1...')
-        C1 = tmi_data.C1()
+        C1 = sofa_data.C1()
 
-        print('Saving TMI_data...')
-        tmi_data.save_data(C1, args.SAVE_DATASET_DIR)
+        print('Saving SOFA_data...')
+        sofa_data.save_data(C1, args.SAVE_DATASET_DIR)
 
         print('Plotting metrics...')
-        tmi_data.plot_metrics(tmi_data.metrics, f'{args.SAVE_DATASET_DIR}/plot_{seq.split("/")[-1]}.png')
+        sofa_data.plot_metrics(sofa_data.metrics, f'{args.SAVE_DATASET_DIR}/plot_{seq.split("/")[-1]}.png')
